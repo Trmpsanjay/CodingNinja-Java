dbc1c640549c97ed695c58c6d1ff5262b198c61f
SANJAY KUMAR <44132320+Trmpsanjay@users.noreply.github.com>
2020-03-13 23:38:28 +0530

    Create Average.java

---
+++ Average.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Average {
+
+
+	public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        System.out.println(name);
+        System.out.print((a+b+c)/3);
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+
+		
+	}
+
+}
