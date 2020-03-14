0e31dbce3f4b68af6f5b78aaf49a82dc76706cc7
SANJAY KUMAR <44132320+Trmpsanjay@users.noreply.github.com>
2020-03-14 10:42:20 +0530

    Create SumOFEvenTillN.java

---
+++ SumOFEvenTillN.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+public class SumOFEvenTillN {
+
+
+	public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=0;
+        for(int i=0;i<=n;i=i+2)
+            sum=sum+i;
+        System.out.print(sum);
+            
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+
+		
+	}
+
+}
